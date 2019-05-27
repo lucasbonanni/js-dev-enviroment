389072054e8550899b44fd59d51b0451b08f0a9a
lucas.bonanni <lucas.bonanni@thomsonreuters.com>
2019-05-27 15:38:42 -0300

    add build scripts and index html

---
+++ build-scripts/src-server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.sendfile(path.join(__dirname,'../src/index.html'));
+});
+
+app.listen(port, function(error){
+  if(error){
+    console.log(error);
+  } else{
+    open('http://localhost:' + port);
+  }
+})
