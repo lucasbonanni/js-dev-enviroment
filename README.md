985c3f3a40564f1e7b2a5f40cda8ea35999b5705
lucas.bonanni <lucas.bonanni@thomsonreuters.com>
2019-05-28 16:39:37 -0300

    Add webpack configuration in the project - read errors in readme

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ### install localtunnel
 npm i localtunnel -g
- (Localtunnel is no working use instead ngrok)
+ (Localtunnel is no working use instead ngrok - test another computer)
+
+
+ ### Errors
+  * Webpack should work without the htmlwebpackplugin but doesn't work - remeber to try this.
--- build-scripts/src-server.js
+++ build-scripts/src-server.js
@@ -1,20 +1,27 @@
-var express = require('express');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var path = require('path');
-var open = require('open');
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
 
-var port = 3000;
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
-var app = express();
-
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname,'../src/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(error){
   if(error){
     console.log(error);
   } else{
+    //console.log(__dirname, config.output.publicPath, config.entry)
     open('http://localhost:' + port);
   }
 })
