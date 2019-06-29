f5ccdfe7935c2a7b7cad60266a10b00aa52fd179
lucas.bonanni <lucas.bonanni@thomsonreuters.com>
2019-06-29 09:41:25 -0300

    changes on style and comments

--- build-scripts/build.js
+++ build-scripts/build.js
@@ -1,9 +1,11 @@
+// More info on Webpack's Node API here: https://webpack.github.io/docs/node.js-api.html
+// Allowing console calls below since this is a build file.
+/*eslint-disable no-console */
 import webpack from 'webpack';
 import webpackConfig from '../webpack.config.prod';
 import chalk from 'chalk';
-import { stat } from 'fs';
 
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'production'; // this assures the Babel dev config doesn't apply.
 
 console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'));
 
--- build-scripts/dist-server.js
+++ build-scripts/dist-server.js
@@ -8,12 +8,11 @@ import compression from 'compression';
 const port = 3000;
 const app = express();
 app.use(compression());
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
-
 
 app.get('/users', function(req, res) {
   res.json([
